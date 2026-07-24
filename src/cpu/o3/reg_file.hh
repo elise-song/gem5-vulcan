ec15c9ab8c7958c6aba9f5d352516999cd9d6017
Elise Song <elise.song@wisc.edu>
2026-07-24 13:58:08 -0500

    cpu-o3: Implement phys regs and free list as statically-allocated structures instead of dynamic

--- src/cpu/o3/reg_file.hh
+++ src/cpu/o3/reg_file.hh
@@ -13,22 +13,33 @@ namespace o3
 
 class RegFile
 {
+    RegVal* pregs;
+    RegVal* freeList[NUM_PREGS];
+    int freeHead, freeTail;
     int freeCount;
 
     void init() {
+        pregs = (RegVal*) calloc(NUM_PREGS, sizeof(RegVal));
+        freeHead = 0;
+        freeTail = 0;
         freeCount = NUM_PREGS;
+        for (int i = 0; i < NUM_PREGS; i++) {
+            freeList[i] = &pregs[i];
+        }
     }
 
     int rename(int pid, RegId areg) {
         if(freeCount == 0) return STALL;
-        RegVal* preg = (RegVal*) calloc(1, sizeof(RegVal));
+        RegVal* preg = freeList[freeHead];
+        freeHead = (freeHead + 1) % NUM_PREGS;
         freeCount--;
         map[pid][areg] = preg;
         return 0;
     }
 
     void free(RegVal* preg) {
-        ::free(preg);
+        freeList[freeTail] = preg;
+        freeTail = (freeTail + 1) % NUM_PREGS;
         freeCount++;
     }
 };
