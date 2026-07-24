c1445d632f64f501bde8d5e34b10847d42c73f6b
Elise Song <elise.song@wisc.edu>
2026-07-23 19:28:18 -0500

    example 1

---
+++ src/mem/cache/private_cache.hh
@@ -0,0 +1,20 @@
+#include "mem/cache/cache.hh"
+
+#ifndef __PRIVATE_CACHE_HH__
+#define __PRIVATE_CACHE_HH__
+
+namespace gem5
+{
+class PrivateCache : public Cache {
+
+    void lookup (Addr vLineAddr, int pid) {
+        int offset = vLineAddr & ((1 << OFFSET_BITS) - 1);
+        int vPage = vLineAddr >> OFFSET_BITS;
+        int pPage = pageLookup(vPage, pid);
+        Addr pLineAddr = (((pPage << OFFSET_BITS) + offset) << PID_BITS) + pid;
+        int tag = pLineAddr >> SET_BITS;
+        int set = pLineAddr & ((1 << SET_BITS) - 1);
+    }
+};
+}
+#endif // __PRIVATE_CACHE_HH__
